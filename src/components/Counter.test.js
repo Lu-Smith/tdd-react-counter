909ffd7bb31ca19b5cd8be63548e638019158b5a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-31 17:40:59 +0000

    Updated Counter.test.js

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -1,3 +1,11 @@
 test('it should have the correct initial value when set to 7', () => {
     throw new Error();
+});
+
+test('it should have a default initial value of 0', () => {
+    throw new Error();
+})
+
+test('it should increase the value correctly when add is clicked once', () => {
+    throw new Error();
 })
