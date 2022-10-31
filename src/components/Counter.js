fbaffb68178b2757a93935917e38ad3599b07fa3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-31 18:46:09 +0000

    Updated Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,11 +5,17 @@ const Counter = ({initialValue}) => {
     const [count, setCount] = useState(initialValue ?? 0);
 
     const add = () => {
-      setCount((prevCount)=>prevCount + 1);
+      setCount((prevCount) => prevCount + 1);
     };
 
     const remove = () => {
-        setCount((prevCount)=>prevCount - 1);
+        setCount((prevCount) => {
+            const result = prevCount - 1;
+            if(result < 0) {
+                return 0;
+            }
+            return result;
+        });
       };
 
   return (
