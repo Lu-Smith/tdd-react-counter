e29f6438580b0d2b49dc74c36e4f176b14b45b90
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-31 19:24:46 +0000

    Created Multiplication.test.js

---
+++ src/components/Multiplication.test.js
@@ -0,0 +1,8 @@
+import {render, screen} from "@testing-library/react";
+import Multiplication from "./Multiplication";
+
+test('it should have the correct initial value when set to 5', () => {
+    render(<Multiplication initialValue={5} />);
+    const count = screen.queryByText(5);
+    expect(count).toBeVisible();
+});
