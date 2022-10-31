4beac2837f82cede9861bfdc6aa9d975e770ac73
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-31 18:06:41 +0000

    Updated Counter.test.js

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -9,8 +9,9 @@ test('it should have the correct initial value when set to 7', () => {
 });
 
 test('it should have a default initial value of 0', () => {
-    throw new Error();
-})
+    render(<Counter />);
+    const count = screen.queryByText(0);
+    expect(count).toBeVisible();
 
 test('it should increase the value correctly when add is clicked once', () => {
     throw new Error();
