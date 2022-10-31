6b7f860c256b86574cc914d17420e0fceca9a326
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-31 20:05:52 +0000

    Updated Multiplication.test.js

--- src/components/Multiplication.test.js
+++ src/components/Multiplication.test.js
@@ -8,3 +8,9 @@ test('it should have the correct initial value when set to 5', () => {
     expect(count).toBeVisible();
 });
 
+test('it should have a default value when set to 0', () => {
+    render(<Multiplication />);
+    const count = screen.queryByText(0);
+    expect(count).toBeVisible();
+});
+
