60debf8e3ffa4461b4a956b5823f74cb0f85e59c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-31 20:34:56 +0000

    Updated Multiplication.js

--- src/components/Multiplication.js
+++ src/components/Multiplication.js
@@ -3,6 +3,13 @@ import "./styles/Multiplication.css";
 
 const Multiplication = ({initialValue}) => {
 
+    const [number, setNumber] = useState(initialValue ?? 0);
+    const [count, setCount] = useState(number);
+   
+
+    const multiply = () => {
+        setCount(number * 2);
+    };
 
 
   return (
@@ -10,11 +17,12 @@ const Multiplication = ({initialValue}) => {
         <h1>Multiplication x 2</h1>
         <div className='container'>
             <div>
-               <input type="number" value={initialValue ?? 0}/>
+               <input type="number" value={number} onChange={event => setNumber(event.target.value)}/>
                <span>X 2</span>
             </div>
             <div>
-               <span>{initialValue ?? 0}</span> 
+               <span>{count}</span>
+               <button onClick={multiply}>Multiply</button>
             </div>
         </div>
     </div>
