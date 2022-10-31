4372bd4902b3d9f2c5b1fbf6b339a7d6e0bb32a4
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-31 19:10:30 +0000

    Created Multiplication.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Counter from './components/Counter';
+import Multiplication from './components/Multiplication';
 
 function App() {
   return (
     <div className='App'>
       <Counter />
+      <Multiplication />
     </div>
   );
 }
