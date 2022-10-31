149c767191aa1ae8ccd4be5ea15fb4cc15bf0c6a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-31 19:33:30 +0000

    Updated Multiplication.test.js

--- src/components/Multiplication.test.js
+++ src/components/Multiplication.test.js
@@ -1,5 +1,6 @@
 import {render, screen} from "@testing-library/react";
 import Multiplication from "./Multiplication";
+import userEvent from "@testing-library/user-event";
 
 test('it should have the correct initial value when set to 5', () => {
     render(<Multiplication initialValue={5} />);
@@ -7,8 +8,15 @@ test('it should have the correct initial value when set to 5', () => {
     expect(count).toBeVisible();
 });
 
-test('it should have andefault value set to 0', () => {
+test('it should have a default value set to 0', () => {
     render(<Multiplication />);
     const count = screen.queryByText(0);
+    expect(count).toBeVisible();
+});
+
+test('it should increase value correctly when click multiply', () => {
+    render(<Multiplication initialValue={2} />);
+    const multiplyButton = screen.getByText("Multiply");
+    const count = screen.queryByText(4);
     expect(count).toBeVisible();
 });
